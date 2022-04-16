5680ea446dfd931a9b092d471014dc284c8d045a
Nahidujjaman Hridoy <hridoyboss12@gmail.com>
2022-04-17 01:43:25 +0600

    Portfolio

--- src/components/Portfolios/Portfolios.js
+++ src/components/Portfolios/Portfolios.js
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useEffect } from "react";
+import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
 
 const Portfolios = () => {
-  return <div>Portfolios</div>;
+  const [images, setImages] = React.useState([]);
+  useEffect(() => {
+    fetch("images.json")
+      .then((response) => response.json())
+      .then((data) => setImages(data));
+  }, []);
+
+  return (
+    <div className="p-4">
+      <h1 className="text-5xl font-semibold text-center m-4 mb-8">
+        Portfolios
+      </h1>
+
+      <ResponsiveMasonry columnsCountBreakPoints={{ 350: 1, 750: 2, 900: 3 }}>
+        <Masonry gutter="10px">
+          {images.map((image) => (
+            <div
+              key={image.id}
+              className=" p-4 bg-white rounded-lg shadow-lg"
+              // className={image.id === 6 ? "col-span-2" : "col-span-1"}
+            >
+              <img src={image.image} alt="" className="w-full" />
+            </div>
+          ))}
+        </Masonry>
+      </ResponsiveMasonry>
+    </div>
+  );
 };
 
 export default Portfolios;
