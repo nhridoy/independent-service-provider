22debfe6d91dc9ee3eee38a27e7259a47836c106
Nahidujjaman Hridoy <hridoyboss12@gmail.com>
2022-04-18 00:38:30 +0600

    404

--- src/pages/NotFound/NotFound.js
+++ src/pages/NotFound/NotFound.js
@@ -1,7 +1,18 @@
 import React from "react";
 
 const NotFound = () => {
-  return <div>NotFound</div>;
+  return (
+    <div>
+      <div className="relative z-0 top-0 h-[40vh] flex items-center justify-center  bg-gray-700 w-full p-4">
+        <h1 className="text-white text-5xl">404 Not Found</h1>
+      </div>
+      <img
+        src="https://error404.fun/img/illustrations/16.png"
+        className="m-auto w-auto"
+        alt=""
+      />
+    </div>
+  );
 };
 
 export default NotFound;
