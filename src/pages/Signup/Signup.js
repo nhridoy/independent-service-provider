04a3ca7a90cefb978118b5824784e37699b481d1
Nahidujjaman Hridoy <hridoyboss12@gmail.com>
2022-04-17 23:36:04 +0600

    Sign up with email and verify email

--- src/pages/Signup/Signup.js
+++ src/pages/Signup/Signup.js
@@ -1,8 +1,44 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import {
+  useCreateUserWithEmailAndPassword,
+  useUpdateProfile,
+} from "react-firebase-hooks/auth";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import GoogleSign from "../../components/GoogleSign/GoogleSign";
+import auth from "../../firebase.init";
 
 const Signup = () => {
+  const nameRef = React.createRef();
+  const emailRef = React.createRef();
+  const phoneRef = React.createRef();
+  const passwordRef = React.createRef();
+
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const [createUserWithEmailAndPassword, user, loading, error] =
+    useCreateUserWithEmailAndPassword(auth, {
+      sendEmailVerification: true,
+    });
+  const [updateProfile, updating, UpdateError] = useUpdateProfile(auth);
+
+  let from = location.state?.from?.pathname || "/";
+
+  if (user) {
+    navigate(from, { replace: true });
+  }
+
+  const handleSignUp = async (e) => {
+    e.preventDefault();
+    const name = nameRef.current.value;
+    const email = emailRef.current.value;
+    const phone = phoneRef.current.value;
+    console.log(phone);
+    const password = passwordRef.current.value;
+    await createUserWithEmailAndPassword(email, password);
+    await updateProfile({ displayName: name, phoneNumber: phone });
+  };
+  console.log(user);
   return (
     <div>
       <div className="relative z-0 top-0 h-[40vh] flex items-center justify-center  bg-gray-700 w-full p-4">
@@ -12,7 +48,7 @@ const Signup = () => {
         <div className="flex justify-center p-4">
           <GoogleSign />
         </div>
-        <form className="p-4">
+        <form className="p-4" onSubmit={handleSignUp}>
           <div className="flex flex-col gap-4 md:flex-row">
             <div className="flex flex-col md:w-1/2">
               <label className="text-gray-600">
@@ -21,6 +57,7 @@ const Signup = () => {
                   className="border border-gray-600 p-2 w-full"
                   type="text"
                   placeholder="Name"
+                  ref={nameRef}
                   required
                 />
               </label>
@@ -32,6 +69,7 @@ const Signup = () => {
                   className="border border-gray-600 p-2 w-full"
                   type="email"
                   placeholder="Email"
+                  ref={emailRef}
                   required
                 />
               </label>
@@ -43,8 +81,9 @@ const Signup = () => {
                 <span className="text-gray-600">Phone</span>
                 <input
                   className="border border-gray-600 p-2 w-full"
-                  type="text"
+                  type="tel"
                   placeholder="Phone"
+                  ref={phoneRef}
                   required
                 />
               </label>
@@ -57,14 +96,22 @@ const Signup = () => {
                   className="border border-gray-600 p-2 w-full"
                   type="password"
                   placeholder="Password"
+                  ref={passwordRef}
                   required
                 />
               </label>
             </div>
           </div>
-
+          {(error || UpdateError) && (
+            <div className="text-red-500 text-sm">
+              {error?.message || UpdateError?.message}
+            </div>
+          )}
           <div className="flex flex-col gap-4 mt-4">
-            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            <button
+              type="submit"
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            >
               Sign Up
             </button>
 
