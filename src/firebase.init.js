cd17153bf223a333239c640c7e3fd53a54913491
Nahidujjaman Hridoy <hridoyboss12@gmail.com>
2022-04-18 00:44:16 +0600

    env

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -8,13 +8,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDaUdvaH8aqtlXvf-tDqHbp7qfO0lJU9JY",
-  authDomain: "elegant-photographer.firebaseapp.com",
-  projectId: "elegant-photographer",
-  storageBucket: "elegant-photographer.appspot.com",
-  messagingSenderId: "1062096932132",
-  appId: "1:1062096932132:web:a6300ade93fb66f93a37b1",
-  measurementId: "G-ZVW25LB6VY",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
